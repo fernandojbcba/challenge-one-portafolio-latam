e6131cc24fb08e9569a77501a12f084bda45b639
fernandojbcba <benavidez.fernando@hotmail.com>
2023-03-08 21:00:13 -0300

    responsive termiando, falta js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,9 @@
 //Haz tú validación en javascript acá
+
+const nombre = document.getElementById ("form_nombre");
+const email = document.getElementById ("form_email");
+const asunto = document.getElementById ("form_asunto");
+const mensaje = document.getElementById ("form_mensaje");
+const botonEnviarFormulario = document.getElementById ("form_boton_enviar");
+
+
